3e89954521190040db4d4bd78a7bf4bb9f7b86eb
Yuliana Alvarado <yalvarado924@gmail.com>
2020-11-11 13:59:48 -0600

    commit tree 3fb45f74213596a8597f634a6816abf811b51855

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+       "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name| 
+        badge_maker(name)
+    end
+end
+
+def assign_rooms(speakers)
+    new_array = []
+    speakers.each_with_index do |name, i|
+        new_array << "Hello, #{name}! You'll be assigned to room #{i+ 1}!"
+    end
+    new_array
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |batch|
+        puts batch
+    end
+    assign_rooms(attendees).each do |rooms|
+        puts rooms
+    end
+end
